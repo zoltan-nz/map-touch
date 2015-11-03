0d89b2b5a276bda5d14d03487f51afd469be73c9
Zoltan <zdebre@gmail.com>
2015-11-04 01:39:16 +1300

    Playing with resize

--- app/components/body-area.js
+++ app/components/body-area.js
@@ -6,24 +6,44 @@ export default Ember.Component.extend({
 
   classNames: ['grey-background'],
 
+  ___resizeFunction : null,
+
   style: Ember.computed('windowWidth', 'windowHeight', function () {
     let width = this.get('windowWidth');
     let height = this.get('windowHeight');
+
+    this.sendAction('windowResized', width, height);
 
     // http://emberjs.com/deprecations/v1.x/#toc_binding-style-attributes
     return new Ember.Handlebars.SafeString(`width: ${width}px; height: ${height}px`);
   }),
 
   windowWidth: Ember.computed('', function () {
-    return $(document).width();
+    return Ember.$(window).width();
   }),
 
   windowHeight: Ember.computed('', function () {
-    return $(document).height();
+    return Ember.$(window).height();
+  }),
+
+  windowResized: Ember.computed('windowWidth', 'windowHeight', function() {
+    debugger;
+    return this.sendAction('windowResized', this.get('windowWidth'), this.get('windowHeight'));
+  }),
+
+  init(...args) {
+    this._super(...args);
+    Ember.$(window).on('resize', function(e) {
+      this.windowChanged().notifyPropertyChange();
+      this.get('style');
+    }.bind(this) );
+  },
+
+  windowChanged: Ember.observer('windowHeight', 'windowWidth', function() {
+    this.get('style');
   }),
 
   click(e) {
-    debugger;
   },
 
   mouseMove(e) {
@@ -31,6 +51,21 @@ export default Ember.Component.extend({
     let y = e.clientY;
 
     this.sendAction('mouseMoved', x, y);
+  },
+
+  touchMove(e) {
+    let x = e.originalEvent.touches[0].clientX;
+    let y =e.originalEvent.touches[0].clientY;
+
+    this.sendAction('mouseMoved', x, y);
+  },
+
+  touchStart(e) {
+    console.log('touch start', e);
+  },
+
+  touchEnd(e) {
+    console.log('touch end', e);
   }
 
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,10 +5,18 @@ export default Ember.Controller.extend({
   mouseX: 0,
   mouseY: 0,
 
+  windowWidth: 0,
+  windowHeight: 0,
+
   actions: {
     mouseMoved(x,y) {
       this.set('mouseX', x);
       this.set('mouseY', y);
+    },
+
+    windowResized(x,y) {
+      this.set('windowWidth', x);
+      this.set('windowHeight', y);
     }
   }
 
