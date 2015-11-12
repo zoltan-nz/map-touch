8b18448c558fceab1de568c9a4a0ab578042e46f
Zoltan <zdebre@gmail.com>
2015-11-13 02:41:36 +1300

    The box is draggable

--- app/components/movable-box.js
+++ app/components/movable-box.js
@@ -11,7 +11,9 @@ export default Ember.Component.extend({
   x: 300,
   y: 300,
 
-  attributeBindings: ['style'],
+  draggable: true,
+
+  attributeBindings: ['style', 'draggable'],
 
   style: computed('width', 'height', 'x', 'y', function () {
     const width = this.get('width');
@@ -21,6 +23,21 @@ export default Ember.Component.extend({
 
     // http://emberjs.com/deprecations/v1.x/#toc_binding-style-attributes
     return new SafeString(`width: ${width}px; height: ${height}px; position: fixed; top:${y}px; left:${x}px`);
-  })
+  }),
 
+  drag(e) {
+    // Response the position of this component e.g.: {top: 5, left: 5}
+    const pos = this.$().position();
+
+    const clientX = parseInt(e.originalEvent.clientX);
+    const clientY = parseInt(e.originalEvent.clientY);
+
+    this.set('x', clientX);
+    this.set('y', clientY);
+    return false;
+  },
+
+  dragOver(e) {
+    return false;
+  }
 });
