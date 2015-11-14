7984fc15ba69230daf35de4eb846eb4bca1ca99c
Zoltan <zdebre@gmail.com>
2015-11-14 19:34:35 +1300

    Moving box working in Chrome only

--- app/components/movable-box.js
+++ app/components/movable-box.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { computed, Handlebars: {SafeString} } = Ember;
+const { computed, Handlebars: {SafeString}, run: {once} } = Ember;
 
 export default Ember.Component.extend({
 
@@ -25,19 +25,55 @@ export default Ember.Component.extend({
     return new SafeString(`width: ${width}px; height: ${height}px; position: fixed; top:${y}px; left:${x}px`);
   }),
 
-  drag(e) {
+  // Great article about drag events: http://tutorials.jenkov.com/html5/drag-and-drop.html
+  dragStart(e) {
+    var dragImage = document.createElement('img');
+    dragImage.width = 1;
+    e.originalEvent.dataTransfer.setDragImage(dragImage, 0, 0);
+
+    e.originalEvent.dataTransfer.effectsAllowed = 'none';
+    e.originalEvent.dataTransfer.dropEffect = 'none';
+
     // Response the position of this component e.g.: {top: 5, left: 5}
     const pos = this.$().position();
 
     const clientX = parseInt(e.originalEvent.clientX);
     const clientY = parseInt(e.originalEvent.clientY);
 
-    this.set('x', clientX);
-    this.set('y', clientY);
+    // Calculate distance between mouse pointer and
+    // the corner position of this div component.
+    const offsetX = clientX - pos.left;
+    const offsetY = clientY - pos.top;
+
+    this.set('offsetX', offsetX);
+    this.set('offsetY', offsetY);
+  },
+
+  drag(e) {
+    const clientX = parseInt(e.originalEvent.clientX);
+    const clientY = parseInt(e.originalEvent.clientY);
+
+    // Ember.run.once update the position only once in
+    // this run loop.
+    once(() => this.updatePosition(clientX, clientY));
+  },
+
+  dragOver() {
     return false;
   },
 
-  dragOver(e) {
+  dragEnd() {
     return false;
+  },
+
+  updatePosition(clientX, clientY) {
+    const offsetX = this.get('offsetX');
+    const offsetY = this.get('offsetY');
+
+    const newX = clientX - offsetX;
+    const newY = clientY - offsetY;
+
+    this.set('x', newX);
+    this.set('y', newY);
   }
 });
